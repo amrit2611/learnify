e33a74e7d31427a83644b913fc641e55c4843a10
amrit <amrit@aviac.in>
2025-01-30 22:34:17 +0530

    some updates

--- db.js
+++ db.js
@@ -3,36 +3,42 @@ const { Schema } = require('mongoose')
 const ObjectId = mongoose.Types.ObjectId
 require('dotenv').config();
 
+
 const userSchema = new Schema({
-    email: { type: String, unique: true },
-    password: String,
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true},
     firstName: String,
     lastName: String,
 })
 
+
 const adminSchema = new Schema({
-    email: { type: String, unique: true },
-    password: String,
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true},
     firstName: String,
     lastName: String,
 })
+
 
 const courseSchema = new Schema({
     title: String,
     description: String,
     price: Number,
     imageUrl: String,
-    creatorId: ObjectId,
+    creatorId: mongoose.Schema.Types.ObjectId,
 })
 
+
 const purchaseSchema = new Schema({
-    userId: ObjectId,
-    courseId: ObjectId,
+    userId: mongoose.Schema.Types.ObjectId,
+    courseId: mongoose.Schema.Types.ObjectId,
 })
+
 
 const userModel = mongoose.model('user', userSchema)
 const adminModel = mongoose.model('admin', adminSchema)
 const courseModel = mongoose.model('course', courseSchema)
 const purchaseModel = mongoose.model('purchase', purchaseSchema)
+
 
 module.exports = { userModel, adminModel, courseModel, purchaseModel }
