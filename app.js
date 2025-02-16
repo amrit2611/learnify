07e02fd1cee087482336f4a3793471f6ce4c3bc5
amrit <amrit@aviac.in>
2025-02-16 21:49:12 +0530

    added the middleware in app.js and updated signin logic

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const { userRouter } = require('./routes/user.js')
+const { userRouter, userVerifiedRouter } = require('./routes/user.js')
 const { courseRouter } = require('./routes/course.js')
 const { adminRouter } = require('./routes/admin.js')
+const { userMiddleware } = require('./middleware/user.js')
 
 require('dotenv').config();
 const app = express();
 app.use(express.json())
 
-app.use('/api/v1/user', userRouter);
+app.use('/user', userRouter);
+app.use('/api/v1/user', userMiddleware, userVerifiedRouter);
 app.use('/api/v1/course', courseRouter);
 app.use('/api/v1/admin', adminRouter);
 
